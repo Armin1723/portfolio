ebc5bf29c1a6610d02bf8d8f55be0e9e6f0721fa
Armin1723 <110764802+Armin1723@users.noreply.github.com>
2024-06-16 13:43:23 +0530

    add : gsap to homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd52afcb40573840"; }
+/******/ 		__webpack_require__.h = function() { return "e9da04040a9af380"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
