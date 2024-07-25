2364720ee9f38213562b8f097174177421d3fb47
Uzair Alam <a-8278@kmclu.ac.in>
2024-07-25 22:47:31 +0530

    feature branch

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a005103a051e81ab")
+/******/ 		__webpack_require__.h = () => ("531264afb6a24b90")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
